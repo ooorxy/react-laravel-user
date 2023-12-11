4739a4b0852e85ac2365e3a3958f6949076f94cf
Rafael Souza <rafael.silva@jimibrasil.com.br>
2023-12-11 16:36:12 -0300

    Finalizada a Sidebar e Topbar.

--- resources/js/Componentes/Topbar.jsx
+++ resources/js/Componentes/Topbar.jsx
@@ -1,9 +1,7 @@
 import { usePage } from "@inertiajs/react";
+import {useState} from "react";
 import Darkmode from "@/Componentes/Darkmode.jsx";
 import {Button, Navbar} from "flowbite-react";
-import {useState} from "react";
-import Icon from "@mdi/react";
-import {mdiMenu, mdiWeatherNight, mdiWhiteBalanceSunny} from "@mdi/js";
 import Icone from "@/Componentes/Icone.jsx";
 
 export default function Topbar () {
@@ -16,17 +14,25 @@ export default function Topbar () {
     }
 
     return <>
-        <Navbar fluid className="py-4 px-2 bg-zinc-100 text-dark dark:text-gray-100 dark:bg-zinc-700">
+        <Navbar fluid className="ml-[80px] py-4 px-2 bg-zinc-100 text-dark dark:text-gray-100 dark:bg-zinc-700">
             <Navbar.Brand href={route('dashboard')}>
-                {/*<Button type={"button"} onClick={switchNav} color="light" className="z-10"><Icone iconName={isNavOpen ? 'Close' : 'Menu'}></Icone></Button>*/}
-                AUTH
+                <span className="text-3xl uppercase">Auth</span>
             </Navbar.Brand>
             <div className="md:order-1">
-                <Darkmode
-                    iconLight={<Icone iconName="WhiteBalanceSunny"></Icone>}
-                    iconDark={<Icone iconName="WeatherNight"></Icone>}
-                />
+                <Darkmode iconLight={<Icone iconName="WhiteBalanceSunny"></Icone>}
+                    iconDark={<Icone iconName="WeatherNight"></Icone>}/>
             </div>
         </Navbar>
+
+        <div id="sidebar" className={`fixed left-0 top-0 bg-zinc-200 dark:bg-zinc-700 p-2 h-full overflow-y-auto w-[80px] hover:w-[350px] transition-all ease-in-out self-center ${isNavOpen ? 'w-[350px]' : 'w-[80px]'}`}>
+            <div className="flex justify-end mb-4">
+                <Button type={"button"} onClick={switchNav} color="light" className="z-10"><Icone iconName={isNavOpen ? 'Close' : 'Menu'}></Icone></Button>
+            </div>
+            <div className="flex flex-col items-center">
+                <ul>
+
+                </ul>
+            </div>
+        </div>
     </>
 }
--- resources/js/Layout.jsx
+++ resources/js/Layout.jsx
@@ -4,7 +4,7 @@ import Topbar from "@/Componentes/Topbar.jsx";
 export default function Layout({ children }) {
     return <>
         <Topbar />
-        <main className="px-10 py-6 min-h-screen bg-slate-100 dark:bg-gray-800 dark:text-gray-100">
+        <main className="ml-[80px] px-10 py-6 min-h-screen bg-slate-100 dark:bg-gray-800 dark:text-gray-100">
             {children}
         </main>
     </>
