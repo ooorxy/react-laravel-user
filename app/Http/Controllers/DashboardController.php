7de4b42f2826c347444cc02e332698727f0f406e
Rafael Souza <rafael.silva@jimibrasil.com.br>
2023-12-13 09:20:43 -0300

    Inicio de Paginação e Animações de Tabela

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,18 +10,6 @@ class DashboardController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $users = User::selectRaw("
-            id,
-            name,
-            email,
-            created_at,
-            case when active = true then 'Sim'
-                else 'Não'
-            end as active,
-            image
-        ")
-            ->get();
-
-        return Inertia::render('Dashboard', compact('users'));
+        return Inertia::render('Dashboard');
     }
 }
--- resources/js/Views/Dashboard.jsx
+++ resources/js/Views/Dashboard.jsx
@@ -1,6 +1,6 @@
 import {Head, usePage} from "@inertiajs/react";
 import Icone from "@/Componentes/Icone.jsx";
-import {Breadcrumb, Button, Card, Table} from "flowbite-react";
+import {Avatar, Breadcrumb, Button, Card, Table} from "flowbite-react";
 
 export default function Dashboard() {
     const { users } = usePage().props;
@@ -15,10 +15,9 @@ export default function Dashboard() {
         <Card>
             <h3>Usuários Cadastrados</h3>
 
-            <Table>
+            <Table striped hoverable>
                 <Table.Head>
                     <Table.HeadCell></Table.HeadCell>
-                    <Table.HeadCell>ID</Table.HeadCell>
                     <Table.HeadCell>Usuário</Table.HeadCell>
                     <Table.HeadCell>E-mail</Table.HeadCell>
                     <Table.HeadCell>Ativo</Table.HeadCell>
@@ -26,51 +25,32 @@ export default function Dashboard() {
                     <Table.HeadCell>Ações</Table.HeadCell>
                 </Table.Head>
                 <Table.Body className="divide-y">
-                    {
-                        users.map(user => {
-                            const date = new Date(user.created_at).toLocaleDateString('pt-BR');
-                            return (
-                                <Table.Row key={user.id}>
-                                    <Table.Cell><img className="border-2 border-gray-400 rounded-full" width="40" height="40" src={user.image} alt="user_image"/></Table.Cell>
-                                    <Table.Cell>{user.id}</Table.Cell>
-                                    <Table.Cell>{user.name}</Table.Cell>
-                                    <Table.Cell>{user.email}</Table.Cell>
-                                    <Table.Cell>{user.active}</Table.Cell>
-                                    <Table.Cell>{date}</Table.Cell>
-                                    <Table.Cell>
-                                        <Button.Group>
-                                            <Button color="blue" size="sm" pill>
-                                                <Icone iconName="Pencil" size={.8}/>
-                                            </Button>
-                                            <Button color="failure" size="sm" pill>
-                                                <Icone iconName="Delete" size={.8}/>
-                                            </Button>
-                                        </Button.Group>
-                                    </Table.Cell>
-                                </Table.Row>
-                            )
-                        })
-                    }
+                    {/*{*/}
+                    {/*    users.map(user => {*/}
+                    {/*        const date = new Date(user.created_at).toLocaleDateString('pt-BR');*/}
+                    {/*        return (*/}
+                    {/*            <Table.Row key={user.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">*/}
+                    {/*                <Table.Cell><img className="border-2 border-gray-400 rounded-full" width="40" height="40" src={user.image} alt="user_image"/></Table.Cell>*/}
+                    {/*                <Table.Cell>{user.name}</Table.Cell>*/}
+                    {/*                <Table.Cell>{user.email}</Table.Cell>*/}
+                    {/*                <Table.Cell>{user.active}</Table.Cell>*/}
+                    {/*                <Table.Cell>{date}</Table.Cell>*/}
+                    {/*                <Table.Cell>*/}
+                    {/*                    <Button.Group>*/}
+                    {/*                        <Button color="blue" size="sm" pill>*/}
+                    {/*                            <Icone iconName="Pencil" size={.8}/>*/}
+                    {/*                        </Button>*/}
+                    {/*                        <Button color="failure" size="sm" pill>*/}
+                    {/*                            <Icone iconName="Delete" size={.8}/>*/}
+                    {/*                        </Button>*/}
+                    {/*                    </Button.Group>*/}
+                    {/*                </Table.Cell>*/}
+                    {/*            </Table.Row>*/}
+                    {/*        )*/}
+                    {/*    })*/}
+                    {/*}*/}
                 </Table.Body>
             </Table>
-            <div className="overflow-x-auto">
-                <table>
-                    <thead>
-                    <tr>
-                        <th className="hidden xl:block"></th>
-                        <th>ID</th>
-                        <th>Usuário</th>
-                        <th>E-mail</th>
-                        <th>Ativo</th>
-                        <th>Data de Criação</th>
-                        <th>Ações</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-                </table>
-            </div>
         </Card>
     </>
 }
