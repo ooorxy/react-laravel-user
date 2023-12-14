bf806849d64fb8cadf92ace6598bdaf1677616c2
Rafael Souza <rafael.silva@jimibrasil.com.br>
2023-12-14 11:00:29 -0300

    Ajuste

--- resources/js/Componentes/WaitingTable.jsx
+++ resources/js/Componentes/WaitingTable.jsx
@@ -3,7 +3,7 @@ import {Spinner, Table} from "flowbite-react";
 export default function WaitingTable ({columns}) {
     return (
         <Table.Row id="waitingTable" className="bg-white dark:border-gray-700 dark:bg-gray-800 animate-pulse py-4">
-            <Table.Cell className="text-center" colSpan={columns}>
+            <Table.Cell className="text-center p-[6rem]" colSpan={columns}>
                 <Spinner aria-label="Loading" size="xl"/>
             </Table.Cell>
         </Table.Row>
